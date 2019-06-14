bf86f3bca77447b11e6a9c9dac8ed7497aa459ae
Gabriel <gabriel11.r@hotmail.com>
2019-06-14 16:53:03 -0300

    criar lista no js

---
+++ script.js
@@ -0,0 +1,14 @@
+let botao = document.querySelector("button");
+
+let filmes = [
+    {titulo: "Os Incríveis", diretor: "Brad Bird", genero: "animação"},
+    {titulo: "Meninas malvadas", diretor:"Mark Waters", genero: "comédia"},
+    {titulo: "Jogos Vorazes", diretor: "Gary Ross", genero: "ficção cientifíca"},
+    {titulo: "Wall-e", diretor: "Andrew Staton", genero: "animação"},
+    {titulo: "Vingadores", diretor: "Joss Whedon", genero: "ação"},
+    {titulo: "Jogos Mortais", diretor: "James Wan", genero: "terror"},
+] 
+
+function sumirFilmes (){
+    
+}
